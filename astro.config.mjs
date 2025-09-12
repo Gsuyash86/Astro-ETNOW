05dc7492ba5e48768af32a60715b3afb78b009bd
Suyash.Gupta <suyesh.gupta@SuyeshGupta.local>
2025-09-12 11:44:23 +0530

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [react()],
-  adapter: vercel(),
+  integrations: [],
   async rewrites() {
     return {
       source: '/:category/(.*)-article-:id',
