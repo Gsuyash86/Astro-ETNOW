176003ae691177d470ce81f8959195d5e5d7fcd2
Suyash.Gupta <suyesh.gupta@SuyeshGupta.local>
2025-09-14 19:01:01 +0530

    Header Ad

--- src/middleware.ts
+++ src/middleware.ts
@@ -33,23 +33,24 @@ export const onRequest: MiddlewareHandler = (context, next) => {
   };
 
  
-  let match = path.match(/^\/([^/]+)\/(.+)-(article)-(\d+)$/);
+  let match = path.match(/^\/([^/]+)(?:\/([^/]+))?(?:\/([^/]+))?\/(.+)-(article)-(\d+)$/);
   if (match) {
-    const [_, category, slug, article_type, id] = match;
-    return context.rewrite(
-      appendCommonParams(
-        `/articleshow?category=${category}&slug=${slug}&article_type=${article_type}&id=${id}`
-      )
-    );
+    const [_, category, sub_category, sub_sub_category, slug, article_type, id] = match;
+    let query = `/articleshow?category=${category}&slug=${slug}&article_type=${article_type}&id=${id}`;
+    if (sub_category) query += `&sub_category=${sub_category}`;
+    if (sub_sub_category) query += `&sub_sub_category=${sub_sub_category}`;
+    return context.rewrite(appendCommonParams(query));
   }
-
   // 🔀 Rewrite for AMP articles
-  match = path.match(/^\/([^/]+)\/(.+)-(article)-(\d+)\/amp$/);
+  match = path.match(/^\/([^/]+)(?:\/([^/]+))?(?:\/([^/]+))?\/(.+)-(article)-(\d+)\/amp$/);
   if (match) {
-    const [_, category, slug, article_type, id] = match;
+    const [_, category, sub_category, sub_sub_category, slug, article_type, id] = match;
+    let query = `/amp/articleshow?category=${category}&slug=${slug}&article_type=${article_type}&id=${id}`;
+    if (sub_category) query += `&sub_category=${sub_category}`;
+    if (sub_sub_category) query += `&sub_sub_category=${sub_sub_category}`;
     return context.rewrite(
       appendCommonParams(
-        `/amp/articleshow?category=${category}&slug=${slug}&article_type=${article_type}&id=${id}`
+        query
       )
     );
   }
