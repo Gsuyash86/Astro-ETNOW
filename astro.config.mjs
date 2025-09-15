8dbe9889287f56b4c8023e3ab866e937a9567853
Suyash.Gupta <suyesh.gupta@SuyeshGupta.local>
2025-09-15 16:19:02 +0530

    Fixed some issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
+
+const isProd = process.env.NODE_ENV === "production";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({
-    mode: "standalone", // or "middleware"
+    mode: "standalone",
   }),
   server: {
     port: 3000,
@@ -19,6 +22,26 @@ export default defineConfig({
         "@constant": ["src/constant"],
         "@styles": ["src/styles"],
       },
+    },
+    css: {
+      modules: {
+        generateScopedName: isProd
+          ? "[hash:base64:6]" 
+          : "[name]__[local]___[hash:base64:6]",
+      },
+      preprocessorOptions: {
+        scss: {
+          outputStyle: 'compressed',
+        },
+      },
+    },
+    build: {
+      minify: isProd ? "esbuild" : false,
+      target: "es2017",
+      cssCodeSplit: true,
+    },
+    esbuild: {
+      drop: isProd ? ["console", "debugger"] : [],
     },
   },
 });
